fef2ce13dd735ffd22d99da6221742001b5ef34c
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-06 16:51:07 +0200

    implement homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Homepage = () => {
   return(
-
+    <div className="jumbotron">
+      <h1 className="display-4">Welcome, World!</h1>
+      <p className="lead">This is the biggest shopping website, here you can find everything you are looking for.</p>
+      <hr className="my-4" />
+      <p>If exists we got it! Go ahead and check our products lists.</p>
+      <p className="lead">
+        <a className="btn btn-secondary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 const Navbar = () => {
   return(
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <a className="navbar-brand" href="#">Shopping Cart</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
